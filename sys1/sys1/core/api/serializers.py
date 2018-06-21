d318c5ddbbfb2e5a80973f51efc32b480308acbc
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-06-21 16:01:42 -0300

    Ajusting the Serializer name

--- sys1/sys1/core/api/serializers.py
+++ sys1/sys1/core/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from django_celery_results.models import TaskResult
 
 
-class ContactSerializer(serializers.ModelSerializer):
+class TaskResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = TaskResult
         fields = [
