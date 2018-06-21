6904de7c5b8555bbc63c05b9b5ca01c1e8320459
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-06-21 11:45:16 -0300

    Adding API pagination

--- sys1/sys1/core/api/views.py
+++ sys1/sys1/core/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework import generics, pagination
 from rest_framework.response import Response
 
 from django_celery_results.models import TaskResult
@@ -6,10 +6,16 @@ from django_celery_results.models import TaskResult
 from .serializers import ContactSerializer
 
 
-class ContactAPIView(APIView):
+class CustomPagination(pagination.PageNumberPagination):
+    page_size = 100
+
+
+class ContactAPIView(generics.ListAPIView):
     """ 
     API to return all messages resulted from the task queue
     """
+    pagination_class = CustomPagination
+
     def get(self, request, format=None):
         qs = TaskResult.objects.all()
         serializer = ContactSerializer(qs, many=True)
